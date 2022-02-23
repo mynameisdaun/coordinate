7b3a791466df14eb5befe8e1a4c15d99b1f1acaa
mynameisdaun <daun9870.jung@gmail.com>
2022-02-23 10:10:21 +0900

    feat(Coordinate.java): 좌표 Validation 추가 X_Y좌표_모두_최대_24까지만_입력할_수_있다

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -4,6 +4,9 @@ public class Coordinate {
     public int y;
 
     public Coordinate(int x, int y) {
+        if(x > 24 || x < 0 || y > 24 || y < 0) {
+            throw new IllegalArgumentException("유효한 좌표의 범위는 0 ~ 24 입니다.");
+        }
         this.x = x;
         this.y = y;
     }
