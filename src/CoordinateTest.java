a7b0a1890d880bd933f4ccf65561da31a1707aea
mynameisdaun <daun9870.jung@gmail.com>
2022-02-23 10:10:53 +0900

    test(CoordinateTest.java): 좌표 Validation X_Y좌표_모두_최대_24까지만_입력할_수_있다

---
+++ src/CoordinateTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CoordinateTest {
+
+    @Test
+    public void X_Y좌표_모두_최대_24까지만_입력할_수_있다() throws Exception {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Coordinate(25,1);
+        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Coordinate(1,25);
+        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Coordinate(-1,4);
+        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Coordinate(4,-1);
+        });
+    }
+
+}
