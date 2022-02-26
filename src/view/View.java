f4415bb8a6574b574190962514a0075ce6ac3740
mynameisdaun <daun9870.jung@gmail.com>
2022-02-26 18:30:34 +0900

    feat(View): 유저로부터 좌표 입력받기 기능 추가 사용자가 점에 대한 좌표 정보를 입력하는 메뉴를 구성한다.

---
+++ src/view/View.java
@@ -0,0 +1,38 @@
+package view;
+
+import model.Coordinate;
+
+import java.io.*;
+import java.util.List;
+
+public class View {
+
+    private final BufferedWriter bw;
+    private final BufferedReader br;
+
+    public View(BufferedWriter bw, BufferedReader br) {
+        this.bw = bw;
+        this.br = br;
+    }
+
+    public String getUserInput() throws IOException {
+        String userInput = "";
+        bw.write("좌표를 입력하세요.\n");
+        bw.flush();
+        userInput = br.readLine();
+        closeBuffer();
+        return userInput;
+    }
+
+    public void printResult(double distance) throws IOException {
+        bw.append("두 점 사이 거리는 ");
+        bw.append(distance+"");
+        bw.flush();
+        closeBuffer();
+    }
+
+    private void closeBuffer() throws IOException {
+        br.close();
+        bw.close();
+    }
+}
