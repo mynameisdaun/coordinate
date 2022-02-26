05c8a60f6351db85646b2c318a595d08de9255d0
mynameisdaun <daun9870.jung@gmail.com>
2022-02-26 19:07:08 +0900

    feat(Controller.java, Coordinate.java): 유저 입력으로 부터 좌표 리스트 생성 좌표 정보는 괄호"(", ")"로 둘러쌓여 있으며 쉼표(,)로 x값과 y값을 구분한다.

---
+++ src/controller/Controller.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.Coordinate;
+import view.View;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+public class Controller {
+    private final View view;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public List<Coordinate> createCoordinateList() throws IOException {
+        String userInput = view.getUserInput();
+        String[] split = userInput.split("-");
+        return Arrays.stream(split)
+                .map(Coordinate::createCoordinate)
+                .collect(toList());
+    }
+}
--- src/model/Coordinate.java
+++ src/model/Coordinate.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static util.Util.toInt;
+
 public class Coordinate {
 
     public int x;
@@ -7,10 +12,27 @@ public class Coordinate {
 
     public Coordinate(int x, int y) {
         if(x > 24 || x < 0 || y > 24 || y < 0) {
-            throw new IllegalArgumentException("유효한 좌표의 범위는 0 ~ 24 입니다.");
+            throw new IllegalArgumentException("유효한 좌표 범위가 아닙니다.\n유효한 좌표의 범위는 0 ~ 24 입니다.");
         }
         this.x = x;
         this.y = y;
     }
 
+    public static Coordinate createCoordinate(String str) {
+        String regex = "(\\d+),(\\d+)";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(str);
+        if(!m.find()) {
+            throw new IllegalArgumentException("유효한 좌표가 아닙니다.\n유효한 좌표 형식은 (x,y) 입니다.");
+        }
+        return new Coordinate(toInt(m.group(1)), toInt(m.group(2)));
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
