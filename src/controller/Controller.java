faa53958e8718170c0d295583b661c8cd88a44e4
mynameisdaun <daun9870.jung@gmail.com>
2022-02-26 22:53:43 +0900

    feat(Controller.java): 좌표값을 두 개 입력한 경우, 두 점을 있는 직선으로 가정한다. 좌표값과 좌표값 사이는 '-' 문자로 구분한다.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,13 +6,13 @@ import view.View;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
+import static java.util.Arrays.*;
 import static java.util.stream.Collectors.toList;
 
 public class Controller {
     private final View view;
+    private final String coordinateParser = "-";
 
     public Controller(View view) {
         this.view = view;
@@ -20,11 +20,10 @@ public class Controller {
 
     public List<Coordinate> createCoordinateList() throws IOException {
         String userInput = view.getUserInput();
-        String[] split = userInput.split("-");
         try {
-            return Arrays.stream(split)
-                    .map(Coordinate::createCoordinate)
-                    .collect(toList());
+            return stream(userInput.split(coordinateParser))
+                        .map(Coordinate::createCoordinate)
+                        .collect(toList());
         } catch (Exception e) {
             view.print(e.getMessage());
             return createCoordinateList();
