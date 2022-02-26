2c84444d2ebc55ce7ef366932691aabb91cee56a
mynameisdaun <daun9870.jung@gmail.com>
2022-02-26 22:46:26 +0900

    feat(View.java): 정상적인 좌표값을 입력한 경우, 해당 좌표에 특수문자를 표시한다.

--- src/view/View.java
+++ src/view/View.java
@@ -5,6 +5,9 @@ import model.Coordinate;
 import java.io.*;
 import java.util.List;
 
+import static util.Util.padIntegerLeft;
+import static util.Util.padStringLeft;
+
 public class View {
 
     private final BufferedWriter bw;
@@ -29,7 +32,47 @@ public class View {
         bw.flush();
     }
 
-    private void closeBuffer() throws IOException {
+    public void printPlane(List<Coordinate> coordinateList) {
+        for(int i=24 ; i>=0 ; i--) {
+            for(int j=0 ; j<=24 ; j++) {
+                Coordinate coordinate = new Coordinate(i, j);
+                if(i==0) {
+                    if(j==0) {
+                        System.out.print("  + ");
+                        continue;
+                    }
+                    System.out.print("——");
+                    continue;
+                }
+                if(j==0&&i>0) {
+                    if(i%2 ==0) {
+                        System.out.print(padIntegerLeft(i, 2));
+                    }else {
+                        System.out.print(padStringLeft(" ", 2));
+                    }
+                    System.out.print("|");
+                }
+                if(coordinateList.contains(coordinate)) {
+                    System.out.print(padStringLeft("★", 2));
+                }else {
+                    System.out.print(padStringLeft(" ", 2));
+                }
+            }
+            System.out.println();
+        }
+        for(int i = 0 ; i <= 24 ; i ++) {
+            if(i==0) {
+                System.out.print(" 0  ");
+            }
+            else if(i%2==0) {
+                System.out.print(padIntegerLeft(i, 2));
+            }else {
+                System.out.print(padStringLeft(" ", 2));
+            }
+        }
+    }
+
+    public void closeBuffer() throws IOException {
         br.close();
         bw.close();
     }
